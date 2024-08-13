63a29c500a25200383f20ea330011060a8fcd14b
Ngoc Pham <bngoc1792@gmail.com>
2024-08-13 10:57:48 +0700

    user register

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import "bootstrap-material-design/dist/css/bootstrap-material-design.min.css";
 import TopNav from "@/components/TopNav";
+import { Toaster } from "react-hot-toast";
 
 export const metadata = {
   title: "ShineTech",
@@ -12,7 +13,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <TopNav />
+        
         {children}
+        <Toaster />
       </body>
     </html>
   );
