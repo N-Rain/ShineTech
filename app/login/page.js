04ca2f5ed1b5a8571f77277bf92dbb3d62cf0f59
Ngoc Pham <bngoc1792@gmail.com>
2024-08-13 11:28:00 +0700

    user login

---
+++ app/login/page.js
@@ -0,0 +1,71 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
+
+export default function Login() {
+  const [email, setEmail] = useState("abc@gmail.com");
+  const [password, setPassword] = useState("12345678");
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      //   console.log(name, email, password);
+      const result = await signIn("credentials", {
+        redirect: false,
+        email,
+        password,
+      });
+
+      if (result?.error) {
+        toast.error(result?.error);
+        setLoading(false);
+      } else {
+        toast.success("Logged in successfully");
+        router.push("/");
+      }
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+  };
+  return (
+    <main>
+      <div className="container">
+        <div className="row d-flex justify-content-center align-items-center vh-100">
+          <div className="col-lg-5 shadow bg-light p-5">
+            <h2 className="mb-4 text-center">Login</h2>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="form-control mb-4"
+                placeholder="Enter your email"
+              />
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="form-control mb-4"
+                placeholder="Enter your password"
+              />
+              <button
+                className="btn btn-primary btn-raised"
+                disabled={loading || !email || !password}
+              >
+                {loading ? "Loading..." : "Submit"}
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
