4f1902048a711be10ac22c8afd830ced4cf56763
Ngoc Pham <bngoc1792@gmail.com>
2024-08-13 14:45:02 +0700

    fix register

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,25 @@
+"use client";
 import "./globals.css";
 import "bootstrap-material-design/dist/css/bootstrap-material-design.min.css";
 import TopNav from "@/components/TopNav";
 import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
 
-export const metadata = {
-  title: "ShineTech",
-  description: "Website bán thiết bị điện tử...",
-};
+// export const metadata = {
+//   title: "ShineTech",
+//   description: "Website bán thiết bị điện tử...",
+// };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <TopNav />
-        
-        {children}
-        <Toaster />
-      </body>
+      <SessionProvider>
+        <body>
+          <TopNav />
+          <Toaster />
+          {children}
+        </body>
+      </SessionProvider>
     </html>
   );
 }
